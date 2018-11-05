262a7e7888ef3635ea00b4a18b9d000420905fff
Jakob Runge <sicarius@g4t3.de>
2018-11-05 17:19:48 +0100

    Add websocket.test.sh

---
+++ src/websocket.test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# https://gist.github.com/htp/fbce19069187ec1cc486b594104f01d0
+curl --include \
+     --no-buffer \
+     --header "Connection: Upgrade" \
+     --header "Upgrade: websocket" \
+     --header "Host: example.com:80" \
+     --header "Origin: http://localhost:27374" \
+     --header "Sec-WebSocket-Key: SGVsbG8sIHdvcmxkIQ==" \
+     --header "Sec-WebSocket-Version: 13" \
+     http://localhost:27374/graphql
