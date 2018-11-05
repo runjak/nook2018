77e7bce8684e10eff2fce70949be602bf208c091
Jakob Runge <sicarius@g4t3.de>
2018-11-05 18:45:19 +0100

    Add websocket.test.sh

--- src/websocket.test.sh
+++ src/websocket.test.sh
@@ -5,7 +5,7 @@ curl --include \
      --header "Connection: Upgrade" \
      --header "Upgrade: websocket" \
      --header "Host: example.com:80" \
-     --header "Origin: http://localhost:27374" \
+     --header "Origin: http://localhost:5000" \
      --header "Sec-WebSocket-Key: SGVsbG8sIHdvcmxkIQ==" \
      --header "Sec-WebSocket-Version: 13" \
-     http://localhost:27374/graphql
+     http://localhost:5000/subscriptions
