e46744d8dcad63d816cc3d2ac9997dd7aa295ba2
Jakob Runge <sicarius@g4t3.de>
2018-11-06 17:46:26 +0100

    Implement PostList

--- src/client/src/PostList.js
+++ src/client/src/PostList.js
@@ -1,7 +1,54 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Query } from 'react-apollo';
+import gql from 'graphql-tag';
+
+const query = gql`
+  query PostList {
+    posts {
+      author
+      comment
+    }
+  }
+`;
 
 function PostList() {
-  return 'Test';
+  return (
+    <Query query={query}>
+      {({ loading, error, data }) => {
+        if (loading) {
+          return (
+            <div>Loading PostList…</div>
+          );
+        }
+
+        if (error) {
+          return (
+            <div>
+              Could not load PostList.
+              Error: {JSON.stringify(error)}
+            </div>
+          );
+        }
+
+        const { posts } = data;
+
+        return (
+          <dl>
+            {posts.map(({ author, comment }, index) => (
+              <Fragment>
+                <dt key={`postlist-dt-${index}`}>
+                  {author}:
+                </dt>
+                <dd key={`postlist-dd-${index}`}>
+                  {comment}
+                </dd>
+              </Fragment>
+            ))}
+          </dl>
+        );
+      }}
+    </Query>
+  );
 }
 
 export default PostList;
