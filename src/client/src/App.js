0023544e5c9dab8dc108f369b3294af12d1481e2
Jakob Runge <sicarius@g4t3.de>
2018-11-06 11:52:18 +0100

    Reduce noise in App.js

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -3,7 +3,6 @@ import ApolloClient from 'apollo-client';
 import { ApolloProvider } from 'react-apollo';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 
-import logo from './logo.svg';
 import './App.css';
 import gqlLink from './gqlLink';
 
@@ -17,18 +16,9 @@ function App() {
     <ApolloProvider client={client}>
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
         </header>
       </div>
     </ApolloProvider>
